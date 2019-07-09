59ed98f953bbaad7f3716986649b689473093f8a
Tusar0003 <faisalahmedtusar@gmail.com>
2019-07-09 12:50:37 +0600

    Add: Final Commit

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,38 +2,59 @@ package com.example.aboutme
 
 import android.content.Context
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.inputmethod.InputBinding
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.example.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName : MyName = MyName("Faisal Ahmed")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button_done).setOnClickListener {
+        /**
+         * Binding object is like a layer of glue between the layout and it's views and it's data
+         */
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+//        findViewById<Button>(R.id.button_done).setOnClickListener {
+//            addNickName(it)
+//        }
+        binding.buttonDone.setOnClickListener {
             addNickName(it)
         }
     }
 
     private fun addNickName(view: View) {
-        val nameEditText = findViewById<EditText>(R.id.edit_text_name)
-        val nameTextView = findViewById<TextView>(R.id.text_view_name)
+//        val nameEditText = findViewById<EditText>(R.id.edit_text_name)
+//        val nameTextView = findViewById<TextView>(R.id.text_view_name)
 //        val doneButton = findViewById<Button>(R.id.button_done)
 
-        nameTextView.text = nameEditText.text
-        nameEditText.visibility = View.GONE
-        view.visibility = View.GONE
+        /**
+         * binding.apply is used to make our code easier to read
+         */
+        binding.apply {
+//            binding.textViewName.text = binding.editTextName.text
+            myName?.nickname = edit_text_name.text.toString()
+
+            /**
+             * In order to refresh the ui with new data, invalidate is used.
+             * So that they get recreated with new data
+             */
+            invalidateAll()
+            binding.textViewName.visibility = View.VISIBLE
+            binding.editTextName.visibility = View.GONE
+            view.visibility = View.GONE
+        }
 
         val imn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imn.hideSoftInputFromWindow(view.windowToken, 0)
